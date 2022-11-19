e0d857043e3e3c557e4be0395036c10b805d12ba
mclaralvs <mclaralvs@gmail.com>
2022-11-19 18:21:20 -0300

    ☁: Lista de Redis finalizada.

--- mainRedis.py
+++ mainRedis.py
@@ -9,7 +9,7 @@ from bson.objectid import ObjectId
 
 # connecting to mongodb ✨
 client = pymongo.MongoClient(
-    "mongodb+srv://maria:1234@mclaralvs.ggbjgpw.mongodb.net/mercadolivre")
+    "mongodb+srv://maria:<PASSWORD>@mclaralvs.ggbjgpw.mongodb.net/mercadolivre")
 db = client.test
 
 global mydb
@@ -18,9 +18,9 @@ mydb = client.mercadolivre
 # connecting to redis ✨
 dbRedis = redis.Redis(host='redis-12850.c60.us-west-1-2.ec2.cloud.redislabs.com',
                       port=12850,
-                      password='O4Gy6ZYU3tjsL2JkpznWHeQ9bevrYVwi')
+                      password='<PASSWORD>')
 
-# login function ✨
+# FUNCTION: login ✨
 def login(cpf):
     # finding the collection from mongodb ✨
     mycol = mydb.usuario
@@ -38,36 +38,58 @@ def login(cpf):
     else:
         dbRedis.hset('user:' + user['cpf'], 'status', 'logged in')  
 
-        print(dbRedis.keys())
-
         print(dbRedis.hget('user:' + user['cpf'], 'status'))
 
-# logout function ✨
+# FUNCTION: logout ✨
 def logout(cpf):
+    # finding the collection from mongodb ✨
+    mycol = mydb.usuario
+
+    # finding the user ✨
+    myuser = {"cpf": cpf}
+
+    user = mycol.find_one(myuser)
+
     status = dbRedis.hget('user:' + cpf, 'status')
 
     if (status.decode() == 'logged in'):
         dbRedis.hset('user:' + cpf, 'status', 'logged out')
+
         print(dbRedis.hget('user:' + cpf, 'status'))
+
     else:
         print('User is already logged out')
 
-# transform it into json ✨
-#userObject = dumps(user)
+# FUNCTION: change product price ✨
+def changePrice(id, price):
+    # finding the collection from mongodb ✨
+    mycol = mydb.produto
 
+    product = mycol.find_one(ObjectId(id))
 
-# sends it to redis ✨
-#sendRedis = dbRedis.set('mydb', usersObject)
+    dbRedis.hset('product:' + id, 'preco', price)  
 
-#usersList = json.loads(dbRedis.get('mydb'))
+    print(dbRedis.hget('product:' + id, 'preco')) 
 
-# printing the user
-""" for user in usersList:
-    print(user['nome'])
-    userObject = dumps(user) """
+# FUNCTION: change product status ✨
+def changeStatus(id, status):
+    # finding the collection from mongodb ✨
+    mycol = mydb.produto
 
-#dbRedis.hmset('nome', 'oiii')
-# print(us8er['nome'])
+    product = mycol.find_one(ObjectId(id))
 
+    dbRedis.hset('productStatus:' + id, 'status', status)  
+
+    print(dbRedis.hget('productStatus:' + id, 'status')) 
+
+# commands ✨
+
+## login commands ☁
 login("123.123.123.11")
-#logout("123.123.123.11")
+logout("123.123.123.11")
+
+## change product price command ☁
+changePrice("632cdccb212e49d773492276", 256.64)
+
+## change product status command ☁
+changeStatus("632cdccb212e49d773492276", 'Indisponivel')
