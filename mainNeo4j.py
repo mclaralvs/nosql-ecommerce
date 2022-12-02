6dddcd2fd35c96cac43cdf554c052c2d3dce6d8d
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2022-12-02 09:50:34 -0300

    ☁: Término das funcionalidades de READ.

--- mainNeo4j.py
+++ mainNeo4j.py
@@ -14,12 +14,15 @@ class App:
         self.driver.close()
 
     # USER FUNCTIONS ✨
+    # create ☁
     def createUser(self):
         with self.driver.session(database="neo4j") as session:
             session.execute_write(self._createUser)
 
     @staticmethod
     def _createUser(db):
+        print("\nCREATE USER ✨")
+
         query = (
             "CREATE (object:user { nome: $nome_user, email: $email_user, cpf: $cpf_user, rua: $rua_user, cidade: $cidade_user, estado: $estado_user, numero: $numero_user })"
         )
@@ -45,6 +48,39 @@ class App:
         return [{"object": row["object"]["nome"]["email"]["cpf"]["rua"]["cidade"]["estado"]["numero"]}
                 for row in result]
 
+    # read ☁
+    def readUsers(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readUsers)
+    
+    @staticmethod
+    def _readUsers(db):
+        print("\nREAD ALL USERS ✨")
+
+        query = "MATCH (u:user) RETURN u"
+
+        result = db.run(query)
+        
+        return [print([row]) for row in result]
+
+    def readUser(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readUser)
+
+    @staticmethod
+    def _readUser(db):
+        print("\nREAD USER BY DOCUMENT NUMBER ✨")
+
+        cpf_user = input("Insert the user's document number: ")
+
+        query = "MATCH (u:user) WHERE u.cpf = $cpf_user RETURN u"
+
+        result = db.run(query,
+                        cpf_user=cpf_user
+                        )
+
+        return [print([row]) for row in result]
+
     # VENDOR FUNCTIONS ✨
     # create ☁
     def createVendor(self):
@@ -53,6 +89,8 @@ class App:
 
     @staticmethod
     def _createVendor(db):
+        print("\nCREATE VENDOR ✨")
+
         query = (
             "CREATE (object:vendor { nome: $nome_vendor, email: $email_vendor, cpf: $cpf_vendor, rua: $rua_vendor, cidade: $cidade_vendor, estado: $estado_vendor, numero: $numero_vendor })"
         )
@@ -78,6 +116,38 @@ class App:
         return [{"object": row["object"]["nome"]["email"]["cpf"]["rua"]["cidade"]["estado"]["numero"]}
                 for row in result]
 
+    # read ☁
+    def readVendors(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readVendors)
+    
+    @staticmethod
+    def _readVendors(db):
+        print("\nREAD ALL VENDORS ✨")
+        query = "MATCH (v:vendor) RETURN v"
+
+        result = db.run(query)
+        
+        return [print([row]) for row in result]
+
+    def readVendor(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readVendor)
+
+    @staticmethod
+    def _readVendor(db):
+        print("\nREAD VENDOR BY DOCUMENT NUMBER ✨")
+
+        cpf_vendor = input("Insert the vendor's document number: ")
+
+        query = "MATCH (v:vendor) WHERE v.cpf = $cpf_vendor RETURN v"
+
+        result = db.run(query,
+                        cpf_vendor=cpf_vendor
+                        )
+
+        return [print([row]) for row in result]
+
     # PRODUCT FUNCTIONS ✨
     # create ☁
     def createProduct(self):
@@ -86,6 +156,8 @@ class App:
 
     @staticmethod
     def _createProduct(db):
+        print("\nCREATE PRODUCT ✨")
+
         query = (
             "CREATE (object:product { nome: $nome_product, preco: $preco_product, quantidade: $quantidade_product, status: $status_product, cpfVendor: $cpf_vendor })"
         )
@@ -106,6 +178,39 @@ class App:
 
         return [{"object": row["object"]["nome"]["preco"]["quantidade"]["status"]["cpfVendor"]}
                 for row in result]
+    
+     # read ☁
+    def readProducts(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readProducts)
+    
+    @staticmethod
+    def _readProducts(db):
+        print("\nREAD ALL PRODUCTS ✨")
+
+        query = "MATCH (p:product) RETURN p"
+
+        result = db.run(query)
+        
+        return [print([row]) for row in result]
+
+    def readProduct(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readProduct)
+
+    @staticmethod
+    def _readProduct(db):
+        print("\nREAD PRODUCT BY NAME ✨")
+
+        name_product = input("Insert the product's name: ")
+
+        query = "MATCH (p:product) WHERE p.nome = $name_product RETURN p"
+
+        result = db.run(query,
+                        name_product=name_product
+                        )
+
+        return [print([row]) for row in result]
 
     # PURCHASE FUNCTIONS ✨
     # create ☁
@@ -115,6 +220,8 @@ class App:
 
     @staticmethod
     def _createPurchase(db):
+        print("\nCREATE PURCHASE ✨")
+
         query = (
             "CREATE (object:purchase { status: $status_purchase, formaPagamento: $forma_pagamento, idProduct: $id_product, quantidade: $quantidade_product, cpfVendor: $cpf_vendor, cpfUser: $cpf_user })"
         )
@@ -138,6 +245,57 @@ class App:
         return [{"object": row["object"]["status"]["formaPagamento"]["idProduct"]["quantidade"]["cpfVendor"]["cpfUser"]}
                 for row in result]
 
+    # read ☁
+    def readPurchases(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readPurchases)
+    
+    @staticmethod
+    def _readPurchases(db):
+        print("\nREAD ALL PURCHASES ✨")
+
+        query = "MATCH (p:product) RETURN p"
+
+        result = db.run(query)
+        
+        return [print([row]) for row in result]
+
+    def readPurchase(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._readPurchase)
+
+    @staticmethod
+    def _readPurchase(db):
+        print("\nREAD PURCHASES BY USER'S DOCUMENT NUMBER ✨")
+
+        cpf_user = input("Insert the user's document number: ")
+
+        query = "MATCH (p:purchase) WHERE p.cpfUser = $cpf_user RETURN p"
+
+        result = db.run(query,
+                        cpf_user=cpf_user
+                        )
+
+        return [print([row]) for row in result]
+
+    # RELATIONSHIP FUNCTIONS ✨
+    def createRelationshipVendorProduct(self):
+        with self.driver.session(database="neo4j") as session:
+            session.execute_write(self._createRelationshipVendorProduct)
+
+    @staticmethod
+    def _createRelationshipVendorProduct(db):
+        vendor_cpf = input("Insert the vendor's document number: ")
+        product_name = input("Insert the product's name: ")
+
+        query = ("MATCH (p2:vendor) WHERE p2.cpf = $vendor_cpf MATCH (p1:product) WHERE p1.nome = $product_name CREATE (p1)-[:SELLED_BY]->(p2) RETURN p1, p2")
+
+        result = db.run(query, 
+                        vendor_cpf, 
+                        product_name
+                        )
+
+        return [{"p1": row["p1"]["nome"], "p2": row["p2"]["cpf"]} for row in result]
 
 if __name__ == "__main__":
     # Credentials ✨
@@ -149,14 +307,25 @@ if __name__ == "__main__":
     # FUNCTIONS ✨
     # user ☁
     #app.createUser()
+    #app.readUsers()
+    #app.readUser()
 
     # vendor ☁
     #app.createVendor()
+    #app.readVendors()
+    #app.readVendor()
 
     # product ☁
     #app.createProduct()
+    #app.readProducts()
+    #app.readProduct()
         
     # purchase ☁
-    app.createPurchase()
+    #app.createPurchase()
+    #app.readPurchases()
+    #app.readPurchase()
+
+    # relationships ☁
+    #app.createRelationshipVendorProduct()
 
     app.close()
